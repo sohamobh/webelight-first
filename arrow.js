3f012eef0bbc129be6fb3c52c98e6bf7b13f005f
Soha <soham@webelight.co.in>
2022-02-23 18:23:46 +0530

    This file containes basic arrow function uses.

---
+++ arrow.js
@@ -0,0 +1,6 @@
+//arrow functions provide us an alternative way to write a more concise and shorter syntax compared to the traditional function expression.
+let addition = (x, y) => x + y;
+console.log(addition(10, 20));
+
+greet = (name) => 'Hello ' + name; //curly braces are not required for single line functions, round braces are not required for single argument.
+console.log(greet("Soha"));
