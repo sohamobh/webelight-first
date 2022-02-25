eda32bbd35e97fd5ce43dcf235da67e0d6ea11ee
Soha <soham@webelight.co.in>
2022-02-25 11:14:05 +0530

    24th Feb

--- generators.js
+++ generators.js
@@ -1,6 +1,6 @@
-function* myGenerator() {
+function* myGenerator() { //* indicated that the function is a generator functions
     let i = 0;
-    yield 1;
+    yield 1;  //allows you to pause and resume a generator function 
     yield 2;
     yield 3;
     yield 4;
