ffbc42d58e2e80b24583c95a25547f160861c508
Soha <soham@webelight.co.in>
2022-03-04 16:53:02 +0530

    React day-2/New_Project-Text management

---
+++ src/components/TextForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TextForm() {
+  return (
+    <div>
+        <div class="mb-3">
+        <label for="MyBox" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="MyBox" rows="3"></textarea>
+        </div>
+    </div>
+  )
+}
