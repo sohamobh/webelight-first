33da1d7930acedbaf76002287a61fbdd34a9d08a
Soha <soham@webelight.co.in>
2022-03-08 11:07:23 +0530

    React day-3/New_Project-Text manipulation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 //new project.. text management
 function App() {
   return (
     <>
-   
+      {/*<Navbar title="TextUtils" aboutText = "About TextUtils"/>*/} 
+      {/*<Navbar/>*/}
+      <Navbar title="TextUtils"/>
+      <div className="container my-3">
+         <TextForm heading="Enter the text to analyse below"/>
+      </div>
+
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,16 +5,16 @@ import React, {useState} from 'react'
 export default function TextForm(props) {
 
   const handleUpClick=()=>{
-    console.log("Uppercase was clicked" +text);
+    // console.log("Uppercase was clicked: " +text);
     let newText = text.toUpperCase();
     setText(newText);
   } 
   const handleOnChange=(event)=>{
-    console.log("On change");
+    // console.log("On change");
     setText(event.target.value);
   } 
 
-  const [text, setText] = useState('Enter text here'); //setText is the update function
+  const [text, setText] = useState('Enter text here'); //setText is the update function //this is usestate hook
   // text="new text"; //wrong way
   // setText("New text"); //correct way
   return (
