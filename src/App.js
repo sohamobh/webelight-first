0e99c05df45c601168cda239fbfdc297aab82718
Soha <soham@webelight.co.in>
2022-03-08 15:59:17 +0530

    React day-2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+import logo from "./logo.svg"
+import "./App.css"
+import Navbar from "./components/Navbar"
+import TextForm from "./components/TextForm"
 
 //new project.. text management
 function App() {
-  return (
-    <>
-      {/*<Navbar title="TextUtils" aboutText = "About TextUtils"/>*/} 
-      {/*<Navbar/>*/}
-      <Navbar title="TextUtils"/>
-      <div className="container my-3">
-         <TextForm heading="Enter the text to analyse below"/>
-      </div>
-
-    </>
-  );
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        Navbar
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarSupportedContent"
+                    >
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a
+                                    className="nav-link active"
+                                    aria-current="page"
+                                    href="/"
+                                >
+                                    Home
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">
+                                    AboutUs
+                                </a>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <input
+                                className="form-control me-2"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                            />
+                            <button
+                                className="btn btn-outline-success"
+                                type="submit"
+                            >
+                                Search
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
 }
 
-export default App;
+export default App
